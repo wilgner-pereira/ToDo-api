120198a0637e8392870a2cb24cfa8de3d2b7b50a
Wilgner <wilgnerpr.dev@gmail.com>
2025-02-18 19:05:48 -0300

    frontend refactor

--- src/view/scripts/script.js
+++ src/view/scripts/script.js
@@ -1,51 +1,45 @@
-const url = "http://localhost:8080/task/user/2";
+const tasksEndpoint = "http://localhost:8080/task/user";
 
 function hideLoader() {
     document.getElementById("loading").style.display = "none";
 }
 
 function show(tasks) {
-    let tab = `
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Username</th>
-                <th scope="col">User ID</th>
-            </tr>
-        </thead>
-        <tbody>
-    `;
+    let tab = `<thead>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+        </thead>`;
 
     for (let task of tasks) {
         tab += `
             <tr>
                 <td scope="row">${task.id}</td>
                 <td>${task.description}</td>
-                <td>${task.user.username}</td>
-                <td>${task.user.id}</td>
             </tr>
         `;
     }
 
-    tab += `</tbody>`; // Fechando o tbody corretamente
     document.getElementById("tasks").innerHTML = tab;
 }
 
-async function getAPI(url) {
-    try {
-        const response = await fetch(url, { method: "GET" });
+async function getTasks() {
+    let key = "Authorization";
+    const response = await fetch(tasksEndpoint, {
+        method: "GET",
+        headers: new Headers({
+            Authorization: localStorage.getItem(key),
+        }),
+    });
 
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status} - ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        hideLoader();
-        show(data);
-    } catch (error) {
-        console.error("Erro ao buscar tarefas:", error);
-    }
+    var data = await response.json();
+    console.log(data);
+    if (response) hideLoader();
+    show(data);
 }
 
-getAPI(url);
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (!localStorage.getItem("Authorization"))
+        window.location = "/view/login.html";
+});
+
+getTasks();
